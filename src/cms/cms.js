2c4c9c740d418f320433ec71206b49f1d12f7a8a
Eky Kwan <eky@eky.hk>
2019-08-23 16:03:11 +0800

    🔧 Update CMS config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,6 +2,8 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
+import styles from '../components/all.sass'
+
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
@@ -9,6 +11,8 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
+
+CMS.registerPreviewStyle(styles)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
